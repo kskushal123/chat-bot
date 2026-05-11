78a1e643577d923851928fe40032df5ff756bca6
KSKushal123 <kskushal123456@gmail.com>
2026-05-11 15:55:55 +0530

    Asdasd

---
+++ chat.py
@@ -0,0 +1,28 @@
+import os
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage, UserMessage
+from azure.core.credentials import AzureKeyCredential
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+endpoint = "https://models.github.ai/inference"
+model = "openai/gpt-5"
+token = os.environ["GITHUB_TOKEN"]
+
+client = ChatCompletionsClient(
+    endpoint=endpoint,
+    credential=AzureKeyCredential(token),
+)
+
+response = client.complete(
+    messages=[
+        SystemMessage(""),
+        UserMessage("What is the color of orange? answer in one word"),
+    ],
+    model=model
+)
+
+print(response.choices[0].message.content)
+
